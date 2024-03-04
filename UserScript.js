1952245f88cae196211a372a24e34cfc681d4a1a
R9OKM <141386188+R9OKM@users.noreply.github.com>
2024-03-04 20:25:47 +0800

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -317,7 +317,8 @@ async function hohohub() {
 }
 async function tsuohub() {
     let e = new URL(window.location.href);
-    e.includes("tsuo-script.xyz/getkey?hwid=") && window.location.replace("https://tsuo-script.xyz/getkey?g-recaptcha-response=c")
+    h = window.location.href;
+    h.includes("tsuo-script.xyz/getkey?hwid=") && window.location.replace("https://tsuo-script.xyz/getkey?g-recaptcha-response=c")
     "tsuo-script.xyz" == t.hostname ? window.location.assign(t.href) : (await new Promise((e, t) => {
         GM.xmlHttpRequest({
             method: "GET",
